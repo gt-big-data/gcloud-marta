c2903f914dbc5eead2bc26373fcb0d2482cec16b
Daniel Fridkin <danielfridkin@gmail.com>
2018-10-15 19:32:24 -0400

    I broke it
    It is broken lol

--- endpoints/recent_stops.py
+++ endpoints/recent_stops.py
@@ -1,10 +1,16 @@
 from flask import Blueprint
+from sqlalchemy import create_engine
+from flask_sqlalchemy import declarative_base
+
+engine = create_engine('sqlite:///gtbigdata:@127.0.0.1:3306/', convert_unicode=True, echo=False)
+Base = declarative_base()
+Base.metadata.reflect(engine)
 
 recent_stops = Blueprint("recent-stops", "recent-stops", url_prefix="/recent-stops")
 
 @recent_stops.route("/")
 def all_stops():
-    return "Not implemented"
+    return str(list(Base.metadata.tables.keys()))
 
 @recent_stops.route("/<path:path>")
 def some_stop():
